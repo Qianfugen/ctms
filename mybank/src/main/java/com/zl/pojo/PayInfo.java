749f81268ad475a7d651b3c53725bf5e0f8d5915
xuhaojie <872414012@qq.com>
2019-12-11 17:49:57 +0800

    第二次修改前端

--- mybank/src/main/java/com/zl/pojo/PayInfo.java
+++ mybank/src/main/java/com/zl/pojo/PayInfo.java
@@ -34,4 +34,72 @@ public class PayInfo implements Serializable {
     @JsonFormat(pattern = "yyyy-MM-dd hh-mm-ss")
     @DateTimeFormat(pattern = "yyyy-MM-dd hh-mm-ss")
     private Date infoTime;
+    /**
+     * 借方账号
+     */
+    private String debtor;
+    /**
+     * 借方用户名
+     */
+    private String debtorName;
+
+    public String getCreditorAcc() {
+        return creditorAcc;
+    }
+
+    public void setCreditorAcc(String creditorAcc) {
+        this.creditorAcc = creditorAcc;
+    }
+
+    public String getCreditorName() {
+        return creditorName;
+    }
+
+    public void setCreditorName(String creditorName) {
+        this.creditorName = creditorName;
+    }
+
+    public BigDecimal getFund() {
+        return fund;
+    }
+
+    public void setFund(BigDecimal fund) {
+        this.fund = fund;
+    }
+
+    public Date getInfoTime() {
+        return infoTime;
+    }
+
+    public void setInfoTime(Date infoTime) {
+        this.infoTime = infoTime;
+    }
+
+    public String getDebtor() {
+        return debtor;
+    }
+
+    public void setDebtor(String debtor) {
+        this.debtor = debtor;
+    }
+
+    public String getDebtorName() {
+        return debtorName;
+    }
+
+    public void setDebtorName(String debtorName) {
+        this.debtorName = debtorName;
+    }
+
+    @Override
+    public String toString() {
+        return "PayInfo{" +
+                "creditorAcc='" + creditorAcc + '\'' +
+                ", creditorName='" + creditorName + '\'' +
+                ", fund=" + fund +
+                ", infoTime=" + infoTime +
+                ", debtor='" + debtor + '\'' +
+                ", debtorName='" + debtorName + '\'' +
+                '}';
+    }
 }
